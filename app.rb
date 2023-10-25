63c1ffb1a9a3347cb947e7b7a051e0d5abe5da4b
antwan64 <146212683+antwan64@users.noreply.github.com>
2023-10-25 05:08:05 +0000

    Process Roll fixes

--- app.rb
+++ app.rb
@@ -2,9 +2,7 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
+  erb(:homepage)
   "
 end
 
@@ -12,7 +10,7 @@ end
 get ("/process_roll") do
   @num_dice = params.fetch("dice").to_i
 
-  @num_dice = params.fetch("sides").to_i
+  @num_sides = params.fetch("sides").to_i
 
   @rolls = []
 
